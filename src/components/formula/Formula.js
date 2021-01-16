093edb158362fc7b3a769082f7e4391b016ed47d
Dinis <deniskosovec1995@gmail.com>
2021-01-16 11:36:01 +0300

    Finish base framework

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,28 @@
+import {ExelComponent} from '@core/ExelComponent'
+
+export class Formula extends ExelComponent {
+    static className = 'exel__formula'
+    constructor($root) {
+        super($root, {
+            name: 'Formula',
+            listeners: ['input', 'click']
+        })
+    }
+
+    toHTML() {
+        return `
+            <div class="info">fx</div>
+            <div class="input" contenteditable spellcheck="false"></div>
+        `
+    }
+
+    onInput(event) {
+        console.log('Formula onInput', event.target.textContent.trim())
+        console.log(this.$root)
+        this.destroy()
+    }
+
+    onClick(event) {
+        console.log('Formula onClick', event)
+    }
+}
---
+++ src/components/toolbar/Toolbar.js
@@ -0,0 +1,38 @@
+import {ExelComponent} from '@core/ExelComponent'
+
+export class Toolbar extends ExelComponent {
+    constructor($root) {
+        super($root, {
+            name: 'Toolbar',
+            listeners: ['click']
+        });
+    }
+    static className = 'exel__toolbar'
+
+    toHTML() {
+        return `
+            <div class="button">
+                <i class="material-icons">format_align_left</i>
+            </div>
+            <div class="button">
+                <i class="material-icons">format_align_center</i>
+            </div>
+            <div class="button">
+                <i class="material-icons">format_align_right</i>
+            </div>
+            <div class="button">
+                <i class="material-icons">format_bold</i>
+            </div>
+            <div class="button">
+                <i class="material-icons">format_italic</i>
+            </div>
+            <div class="button">
+                <i class="material-icons">format_underline </i>
+            </div>
+        `
+    }
+
+    onClick() {
+        console.log('toolbarClick')
+    }
+}

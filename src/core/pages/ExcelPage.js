dd34e505d1ea1b0705549fd6410e8be2b0fe7624
Dinis <deniskosovec1995@gmail.com>
2021-01-28 14:54:31 +0300

    create StateProcessor, refactor state to work with server and LocalStorage

--- src/core/pages/ExcelPage.js
+++ src/core/pages/ExcelPage.js
@@ -1,28 +1,27 @@
-import {Page} from '@core/Page'
+import {Page} from '@core/pages/Page'
 import {createStore} from '@core/store/createStore'
 import {rootReducer} from '@/redux/rootReducer'
 import {normalizeInitialState} from '@/redux/initialState'
-import {debounce, storage} from '@core/utils'
 import {Excel} from '@/components/excel/Excel'
 import {Header} from '@/components/header/Header'
 import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
-
-function storageName(param) {
-    return 'excel:' + param
-}
+import {StateProcessor} from '@core/pages/StateProcessor'
+import {LocalStorageClient} from '@core/store/LocalStorageClient'
 
 export class ExcelPage extends Page {
-    getRoot() {
-        const params = this.params ? this.params : Date.now().toString()
-        const state = storage(storageName(params))
+    constructor(param) {
+        super(param)
+        this.storeSub = null
+        this.processor = new StateProcessor(
+        new LocalStorageClient(this.params)
+    )
+    }
+    async getRoot() {
+        const state = await this.processor.get()
         const store = createStore(rootReducer, normalizeInitialState(state))
-        const stateListener = debounce(state => {
-            storage(storageName(params), state)
-        }, 300)
-
-        store.subscribe(stateListener)
+        this.storeSub = store.subscribe(this.processor.listen)
 
         this.excel = new Excel({
             components: [Header, Toolbar, Formula, Table],
@@ -37,5 +36,6 @@ export class ExcelPage extends Page {
 
     destroy() {
         this.excel.destroy()
+        this.storeSub.unsubscribe()
     }
 }
--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,6 @@
 import {$} from '../dom'
 import {ActiveRoute} from './ActiveRoute'
+import {Loader} from '../../components/Loader'
 
 export class Router {
     constructor(selector, routes) {
@@ -7,6 +8,7 @@ export class Router {
             throw new Error('Selector is not provided in Router')
         }
         this.$placeholder = $(selector)
+        this.loader = new Loader()
         this.routes = routes
         this.page = null
         this.changePageHandler = this.changePageHandler.bind(this)
@@ -18,16 +20,17 @@ export class Router {
         this.changePageHandler()
     }
 
-    changePageHandler(event) {
+    async changePageHandler(event) {
         if (this.page) {
             this.page.destroy()
         }
+        this.$placeholder.clear().append(this.loader)
         const Page = ActiveRoute.path.includes('excel')
             ? this.routes.excel
             : this.routes.dashboard
         this.page = new Page(ActiveRoute.param)
-        this.$placeholder.clear()
-        this.$placeholder.append(this.page.getRoot())
+        const root = await this.page.getRoot()
+        this.$placeholder.clear().append(root)
         this.page.afterRender()
     }
 
